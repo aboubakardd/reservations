0424b29f389b712193272689ea2a111f2f5314bd
aboubakardaouda15@gmail.com <aboubakardaouda15@gmail.com>
2024-04-03 16:46:05 +0200

    Controller Artist +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ArtistController;
+use App\Models\Artist;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist.index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+		->where('id', '[0-9]+')->name('artist.show');
