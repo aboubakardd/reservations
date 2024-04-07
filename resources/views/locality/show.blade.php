030003688021dc1c67b93f8f709fd8b212978797
aboubakardaouda15@gmail.com <aboubakardaouda15@gmail.com>
2024-04-07 14:36:55 +0200

    Template Locality/Show

--- resources/views/locality/show.blade.php
+++ resources/views/locality/show.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Fiche d\'une localité')
 
 @section('content')
-    <h1>{{ $locality->locality }}</h1>
+    <h1>{{ $locality->postal_code }} {{ $locality->locality }}</h1> 
+        <ul>
+            @foreach($locality->locations as $location)
+                <li>{{ $location->designation }}</li>
+            @endforeach
+        </ul>
+
     <nav><a href="{{ route('locality_index') }}">Retour à l'index</a></nav>
 @endsection
