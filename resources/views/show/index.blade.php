b6a3cb90356d9087fd159f76999a77c6c0238629
aboubakardaouda15@gmail.com <aboubakardaouda15@gmail.com>
2024-04-08 15:05:01 +0200

    Templates Show: affichage des représentations

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -8,11 +8,20 @@
     <ul>
     @foreach($shows as $show)
     <li>
-        <a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a>
+    <a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a>
         @if($show->bookable)
         <span>{{ $show->price }} €</span>
+        @endif
+        
+        @if($show->representations->count()==1)
+        - <span>1 représentation</span>
+        @elseif($show->representations->count()>1)
+        - <span>{{ $show->representations->count() }} représentations</span>
+        @else
+        - <em>aucune représentation</em>
         @endif
     </li>
     @endforeach
     </ul>
 @endsection
+
