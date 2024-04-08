c4c46b39cd3bc672291be3d30f3f28bf50246aec
aboubakardaouda15@gmail.com <aboubakardaouda15@gmail.com>
2024-04-08 13:56:13 +0200

    Controller Representation +templates +routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ShowController;
+use App\Http\Controllers\RepresentationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -64,6 +65,12 @@ Route::get('/location/{id}', [LocationController::class, 'show'])
 Route::get('/show', [ShowController::class, 'index'])->name('show.index');
 Route::get('/show/{id}', [ShowController::class, 'show'])
 ->where('id', '[0-9]+')->name('show.show');
+
+//route pour representantation
+Route::get('/representation', [RepresentationController::class, 'index'])
+->name('representation_index');
+Route::get('/representation/{id}', [RepresentationController::class, 'show'])
+->where('id', '[0-9]+')->name('representation_show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
